18a63e71795267c28f5a371a8580ba55ce1a5a54
Joe Pattarapon <pattarapon131@gmail.com>
2024-04-22 21:02:26 +0700

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/deploying-vite-project/',
+  base: '/deploying-Joe-Pattarapon-Portfolio/',
   plugins: [vue(),svgLoader()],
 })
