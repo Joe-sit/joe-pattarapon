644c28a01bd95805428c0a86efcb0b524049d360
Joe Pattarapon <pattarapon131@gmail.com>
2024-04-27 19:38:56 +0700

    add dist

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import svgLoader from 'vite-svg-loader'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  // base: '/joe-pattarapon',
-  plugins: [vue(),svgLoader()],
-})
